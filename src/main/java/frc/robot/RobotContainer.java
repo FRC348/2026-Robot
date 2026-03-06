49a6068ea7a5bf4aa665189b95b791a3b21abcdb
Team-348-Programming <348progamming@gmail.com>
2026-03-06 16:46:08 -0500

    3/6/26 Smoke Test 1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,30 +32,23 @@ public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
   private static final DriveSubsystem m_robotDrive = new DriveSubsystem();
-  public static final VisionSS rc_visionSS = new VisionSS();
+  //public static final VisionSS rc_visionSS = new VisionSS();
   public static final IntakeTiltSS rc_IntakeTiltSS = new IntakeTiltSS();
   public static final IntakeSS rc_intakeSS = new IntakeSS();
-<<<<<<< HEAD
-  public static final KickerSS rc_KickerSS = new KickerSS();
-  /// ???? public static final ClimbPIDSS rc_pidSS = new ClimbPIDSS();
-  
-  public static final IntakeC rc_intakeC = new IntakeC(rc_intakeSS);
-  public static final KickerC rc_KickerC = new KickerC(rc_KickerSS);
-=======
   public static final LauncherSS rc_launcherSS = new LauncherSS();
   public static final ClimbSS rc_climbSS = new ClimbSS();
   public static final KickerSS rc_KickerSS = new KickerSS();
   public static final ChangeModeSS rc_changeModeSS = new ChangeModeSS();
 
   public static final IntakeC rc_intakeC = new IntakeC(rc_intakeSS);
-  public static final LauncherC rc_launcherC = new LauncherC(rc_launcherSS);
+  //public static final LauncherC rc_launcherC = new LauncherC(rc_launcherSS);
   public static final LauncherSpeedC rc_launcherspeedC = new LauncherSpeedC(rc_launcherSS, 0.5);
   public static final ClimbC rc_climbC = new ClimbC(rc_climbSS);
   public static final KickerC rc_KickerC = new KickerC(rc_KickerSS);
   public static final StaticLauncherC rc_staticLauncherC = new StaticLauncherC(rc_launcherSS);
   public static final ChangeModeC rc_changeModeC = new ChangeModeC(rc_changeModeSS);
+  public static final IntakeTiltC rc_intakeTiltC = new IntakeTiltC(rc_IntakeTiltSS);
 
->>>>>>> 6c1d4a50f7f29e43e5f3b01a6d84f32fa4aa5938
   public static final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
   public static final CommandXboxController m_operatorController =
@@ -63,14 +56,14 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-     rc_visionSS.setDefaultCommand(
-      new RunCommand(
-          () ->
-            rc_visionSS.PrintTarget(),
-            rc_visionSS
+    //  rc_visionSS.setDefaultCommand(
+    //   new RunCommand(
+    //       () ->
+    //         rc_visionSS.PrintTarget(),
+    //         rc_visionSS
             
-      )
-    );
+    //   )
+    // );
     // Configure the trigger bindings
     configureBindings();
     // Configure default commands
@@ -110,21 +103,18 @@ public class RobotContainer {
     m_operatorController.a().whileTrue(rc_climbC);
     m_operatorController.b().whileTrue(rc_climbC);
 
-    m_operatorController.povRight().onTrue(new IntakeTiltC(rc_IntakeTiltSS, () -> 10));
-    m_operatorController.povUp().onTrue(new IntakeTiltC(rc_IntakeTiltSS, () -> 5));
-    m_operatorController.povLeft().onTrue(new IntakeTiltC(rc_IntakeTiltSS, () -> 0));
+    m_operatorController.povRight().whileTrue(rc_intakeTiltC);
+    m_operatorController.povLeft().whileTrue(rc_intakeTiltC);
 
     m_operatorController.leftBumper().whileTrue(rc_intakeC);
     m_operatorController.povDown().whileTrue(rc_intakeC);
-    m_operatorController.leftBumper().whileTrue(rc_KickerC);
-    m_operatorController.rightTrigger().whileTrue(rc_KickerC);
     m_operatorController.leftTrigger().whileTrue(rc_KickerC);
-    m_operatorController.povDown().whileTrue(rc_KickerC);
+    m_operatorController.rightTrigger().whileTrue(rc_KickerC);
+    m_operatorController.rightBumper().whileTrue(rc_KickerC);
       
     if (rc_changeModeSS.manual == false) {
-      m_operatorController.leftTrigger().whileTrue(rc_staticLauncherC);
       m_operatorController.rightTrigger().whileTrue(rc_staticLauncherC);
-      m_operatorController.rightBumper().whileTrue(rc_launcherC);
+      //m_operatorController.rightBumper().whileTrue(rc_launcherC);
     }
 
     else if (rc_changeModeSS.manual == true) {
@@ -132,9 +122,8 @@ public class RobotContainer {
       m_driverController.povDown().onTrue(new LauncherSpeedC(rc_launcherSS, -1));
       m_driverController.povRight().onTrue(new LauncherSpeedC(rc_launcherSS, 0.1));
       m_driverController.povLeft().onTrue(new LauncherSpeedC(rc_launcherSS, -0.1));
-      m_driverController.leftTrigger().onTrue(rc_launcherC);
-      m_driverController.rightTrigger().onTrue(rc_launcherC);
-      m_driverController.rightBumper().onTrue(rc_launcherC);
+      //m_driverController.rightTrigger().onTrue(rc_launcherC);
+      //m_driverController.rightBumper().onTrue(rc_launcherC);
     }
 
     // Launcher Testing button binds
--- src/main/java/frc/robot/commands/LauncherC.java
+++ src/main/java/frc/robot/commands/LauncherC.java
@@ -14,45 +14,45 @@ import frc.robot.RobotContainer;
 import frc.robot.subsystems.LauncherSS;
 import frc.robot.subsystems.VisionSS;
 public class LauncherC extends Command {
-    public double speed;
-    List<PhotonPipelineResult> results = RobotContainer.rc_visionSS.camera.getAllUnreadResults();
-    PhotonTrackedTarget target = results.get(0).getBestTarget();
-    double targetHypotenuse = 1;
+    // public double speed;
+    // List<PhotonPipelineResult> results = RobotContainer.rc_visionSS.camera.getAllUnreadResults();
+    // PhotonTrackedTarget target = results.get(0).getBestTarget();
+    // double targetHypotenuse = 1;
 
-    public final double getDistance() {
-        if (results.size() > 0) {
-            final double targetPitchRadians = target.getPitch();
-            targetHypotenuse = PhotonUtils.calculateDistanceToTargetMeters(Constants.VisionConstants.cameraHeightMeters, Constants.VisionConstants.targetHeightMeters, Constants.VisionConstants.cameraPitchRadians, targetPitchRadians);
-        } 
-        return targetHypotenuse;
-    }
+    // public final double getDistance() {
+    //     if (results.size() > 0) {
+    //         final double targetPitchRadians = target.getPitch();
+    //         targetHypotenuse = PhotonUtils.calculateDistanceToTargetMeters(Constants.VisionConstants.cameraHeightMeters, Constants.VisionConstants.targetHeightMeters, Constants.VisionConstants.cameraPitchRadians, targetPitchRadians);
+    //     } 
+    //     return targetHypotenuse;
+    // }
    
 
 
-    public LauncherC(LauncherSS subsystem) {
-        subsystem = RobotContainer.rc_launcherSS;
-        addRequirements(subsystem);
-    }
+    // public LauncherC(LauncherSS subsystem) {
+    //     subsystem = RobotContainer.rc_launcherSS;
+    //     addRequirements(subsystem);
+    // }
 
-    @Override
-    public void initialize() {}
+    // @Override
+    // public void initialize() {}
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-        double launchspeed = RobotContainer.rc_launcherSS.calculateLaunchSpeed(getDistance());
-        RobotContainer.rc_launcherSS.spin(launchspeed);
-    }
+    // // Called every time the scheduler runs while the command is scheduled.
+    // @Override
+    // public void execute() {
+    //     double launchspeed = RobotContainer.rc_launcherSS.calculateLaunchSpeed(getDistance());
+    //     RobotContainer.rc_launcherSS.spin(launchspeed);
+    // }
 
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {
-        RobotContainer.rc_launcherSS.stop();
-    }
+    // // Called once the command ends or is interrupted.
+    // @Override
+    // public void end(boolean interrupted) {
+    //     RobotContainer.rc_launcherSS.stop();
+    // }
 
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+    // // Returns true when the command should end.
+    // @Override
+    // public boolean isFinished() {
+    //     return false;
+    // }
 }
